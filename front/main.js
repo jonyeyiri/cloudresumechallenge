06fe259bebf91c08f350b387287b7d31b257a7f2
John Onyeyiri <johnonyeyiri@gmail.com>
2021-10-16 02:15:51 -0500

    create local function

--- front/main.js
+++ front/main.js
@@ -3,8 +3,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
-const localApi = 'http://localhost:7071/api/GetResumeCounter';
-const functionApi = 'https://getresumecounterfunctionapp.azurewebsites.net/api/GetResumeCounter?code=jp4rx3wuLEzaXvSFeBKaq08RGiVCwRtpAA1Cdxdc15c9ZGjM79tWjg=='; 
+const localApi = '';
+const functionApi = ''; 
 
 const getVisitCount = () => {
     let count = 30;
